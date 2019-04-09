ad104f2c55bc920c4161a29219e73b5aa8939f4e
syYing <706580680@qq.com>
2019-04-09 15:38:45 +0800

    layout example

--- thymeleaf-layout/src/main/java/com/lucien/controller/IndexController.java
+++ thymeleaf-layout/src/main/java/com/lucien/controller/IndexController.java
@@ -11,4 +11,19 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/fragment")
+    public String fragment() {
+        return "fragment";
+    }
+
+    @RequestMapping("/layout")
+    public String layout() {
+        return "layout";
+    }
+
+    @RequestMapping("/home")
+    public String home() {
+        return "home";
+    }
 }
